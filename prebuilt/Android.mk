17fe7bb7465fe6644f253430e9cfca2e16013122
Andreas Fried <AndrolinRoot@googlemail.com>
2014-06-21 16:41:57 +0200

    Try to change to aokp owner

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -49,7 +49,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := UIprov
-LOCAL_MODULE_OWNER := vanilla
+LOCAL_MODULE_OWNER := aokp
 LOCAL_SRC_FILES := common/app/$(LOCAL_MODULE).apk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .apk

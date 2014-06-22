86974e27c1dbe1871c00d869bbab0eb9740a7c54
Cooper Le <koopa@gfxbandits.com>
2014-06-21 23:32:08 -0500

    Update vu_falcon.mk

--- products/vu_falcon.mk
+++ products/vu_falcon.mk
@@ -3,6 +3,12 @@ $(call inherit-product, device/motorola/falcon/full_falcon.mk)
 # Inherit from xt1034 device
 $(call inherit-product, device/motorola/falcon/device_falcon.mk)
 
+# Inherit common gsm config
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := falcon
 PRODUCT_NAME := vu_falcon

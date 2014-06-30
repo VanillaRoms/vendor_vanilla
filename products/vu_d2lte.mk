f4a5e1b5645dd93fe8ebf02e706b8bb618aa7925
isaiahpez <isaiahpez@gmail.com>
2014-06-30 11:57:42 -0400

    aokp-->vu

--- products/vu_d2lte.mk
+++ products/vu_d2lte.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/vanilla/configs/gsm.mk)
 $(call inherit-product, vendor/vanilla/configs/common.mk)
 
 PRODUCT_DEVICE := d2lte
-PRODUCT_NAME := aokp_d2lte
+PRODUCT_NAME := vu_d2lte
 
 PRODUCT_COPY_FILES += \
     vendor/vanilla/prebuilt/bootanimation/bootanimation_720_720.zip:system/media/bootanimation-alt.zip
